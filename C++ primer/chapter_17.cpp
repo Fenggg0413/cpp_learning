6061091ee7ae6313aec0c232c65da3c6b3f43b99
Fen4 <18178712792@163.com>
2023-07-05 14:51:33 +0800

    7-5

---
+++ C++ primer/chapter_17.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <regex>
+using std::regex;
+using std::regex_error;
+
+void test1()
+{
+    // for ex17.14
+    // error_brack
+    try
+    {
+        regex r("[[:alnum:]+\\.(cpp|cxx|cc)$", regex::icase);
+    }
+    catch (regex_error e)
+    {
+        cout << e.what() << " code: " << e.code() << endl;
+    }
+}
+
+void test2()
+{
+    // for ex17.15
+    regex r("[[:alpha:]]*[^c]ei[[:alpha:]]*", regex::icase);
+    string s;
+    cout << "Please input a word! Input 'q' to quit!" << endl;
+    while (cin >> s && s != "q")
+    {
+        if (std::regex_match(s, r))
+            cout << "Input word " << s << " is okay!" << endl;
+        else
+            cout << "Input word " << s << " is not okay!" << endl;
+
+        cout << "Please input a word! Input 'q' to quit!" << endl;
+    }
+}
+
+int main()
+{
+    test1();
+    test2();
+    return 0;
+}
