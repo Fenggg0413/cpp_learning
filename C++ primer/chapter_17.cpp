0ac8e9f6331f30ff83ebe46926d82aba0369aef7
Fen4 <18178712792@163.com>
2023-10-31 11:04:17 +0800

    updata

--- C++ primer/chapter_17.cpp
+++ C++ primer/chapter_17.cpp
@@ -3,6 +3,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#include <fstream>
 #include <string>
 using std::string;
 
@@ -53,9 +54,55 @@ unsigned int myRandomInt(int s, size_t min, size_t max)
     return u(e);
 }
 
+void test3()
+{
+    std::ifstream ifs("temp.txt");
+    if (ifs)
+        cout << 1 << endl;
+    char sink[30];
+    while (ifs.getline(sink, 30))
+        cout << sink << endl;
+    ifs.close();
+}
+
+void test4()
+{
+    // 以可读写方式打开文件并已开始就定位到文件尾
+    std::fstream fs("temp", std::fstream::in | std::fstream::out | std::fstream::ate);
+    if (!fs)
+    {
+        cout << "open file failed" << endl;
+        return;
+    }
+    // 记录原本文件尾的位置
+    auto end_mark = fs.tellg();
+    // 将流重定位到文件开头
+    fs.seekg(0, std::fstream::beg);
+    string line;
+    int count = 0;
+    bool flag = true;
+    while (fs && fs.tellg() != end_mark && getline(fs, line))
+    {
+        count += line.size() + 1; // 记得加上换行符
+        auto mark = fs.tellg();
+        fs.seekp(0, std::fstream::end);
+        if(flag)
+        {
+            fs << endl;
+            flag = false;
+        }
+        fs << count;
+        if(mark != end_mark)
+            fs << " ";
+        fs.seekg(mark);
+    }
+    fs.seekp(0, std::fstream::end);
+    fs << endl;
+    fs.close();
+}
+
 int main()
 {
-    int tmp = myRandomInt(time(0), 0, 100);
-    cout << tmp << endl;
+    test4();
     return 0;
 }
