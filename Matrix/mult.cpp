27e3a97da3cc61e74bca182e1baa3ed215d25abb
Feng <18178712792@163.com>
2023-12-12 11:21:08 +0800

    debug multiply Matrix

--- Matrix/mult.cpp
+++ Matrix/mult.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <utility>
 
-std::vector<std::vector<int>> multiplyMat(std::vector<std::vector<int>> mat1, std::vector<std::vector<int>> mat2)
+#define DEBUG 1
+
+std::vector<std::vector<int>> multiplyMat(const std::vector<std::vector<int>> &mat1, const std::vector<std::vector<int>> &mat2)
 {
     int m = mat1.size();
     int n = mat2[0].size();
@@ -31,8 +33,39 @@ std::vector<std::vector<int>> multiplyMat(std::vector<std::vector<int>> mat1, st
     return std::move(newMat);
 }
 
+void printMat(const std::vector<std::vector<int>> &mat)
+{
+    for(const auto &vec : mat)
+    {
+        for(const auto &val : vec)
+            std::cout << val << " ";
+        std::cout << "\n";
+    }
+}
 int main()
 {
+    if(DEBUG)
+    {
+        std::vector<std::vector<int>> mat1{{0, 1, 0, 0, 0},
+                                           {0, 0, 1, 1, 0},
+                                           {1, 0, 0, 0, 0},
+                                           {0, 0, 1, 0, 0},
+                                           {0, 1, 0, 1, 0}};
+        auto mat1_2 = multiplyMat(mat1, mat1);
+        std::cout << "mat_2: \n";
+        printMat(mat1_2);
+        auto mat1_3 = multiplyMat(mat1, mat1_2);
+        std::cout << "mat_3: \n";
+        printMat(mat1_3);
+        auto mat1_4 = multiplyMat(mat1, mat1_3);
+        std::cout << "mat_4: \n";
+        printMat(mat1_4);
+        auto mat1_5 = multiplyMat(mat1, mat1_4);
+        std::cout << "mat_5: \n";
+        printMat(mat1_5);
+        return 0;
+    }
+    
     int m{}, n{};
     std::cout << "mat1: \n";
     std::cin >> m >> n;
